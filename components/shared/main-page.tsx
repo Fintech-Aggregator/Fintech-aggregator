b3218b5f515422d005b93b81e3f32a8739e8207c
Volodia <volodakuznetsov131@gmail.com>
2025-02-03 17:13:52 +0200

    main-page update

--- components/shared/main-page.tsx
+++ components/shared/main-page.tsx
@@ -1,13 +1,13 @@
 import styles from "./HomeContentTemaplate/main-page.module.css";
 import Image3 from "./images/image3.png";
 import Image2 from "./images/image2.jpg";
+import Image4 from "./images/image4.jpg";
+import Image5 from "./images/image5.png";
 
 export default function Content() {
   return (
     <div className={styles.template}>
       <div className={styles.block1}>
-        
-        {/* <img src={Image1.src} className={styles.image1} /> */}
         
         <div className={styles.text_overlay1}>
           <h1>Your key to fintech licenses Fast, Accurate, Convenient</h1>
@@ -44,11 +44,46 @@ export default function Content() {
         <img src={Image2.src} className={styles.image2} />
         
         <div className={styles.text_overlay2}>
+        The project was completed by a team of 6 people with an average <br/>
+        of XX hours spent by each person. 
+        </div>
+        
+      </div>
+      
+      <div className={styles.text_between}>
+      
+        <div className={styles.containerImage1}>
+          <img src={Image5.src} className={styles.image5}/>
+        </div>
+      
+        <div className={styles.template_between}>
+          <div className={styles.text_bet1}>
+            <h1>Why you should choose us?</h1>
+            
+            <h2>
+            We provide access to data on licenses, owners, types of activities, 
+            and compliance with regulatory requirements which will help increase 
+            market transparency. We make it easier for you to choose your partners, 
+            as you can easily find the information 
+            you need by country, license type of activity, owners, etc.
+            </h2>
+            
+          </div>
+        </div>
+        
+      </div>
+      
+      <div className={styles.block2}>
+        
+        <img src={Image4.src} className={styles.image2} />
+        
+        <div className={styles.text_overlay2}>
           We are a team of enthusiasts who strive to make financial information <br/>
           more accessible, transparent and user-friendly.
         </div>
         
       </div>
+        
     </div>
   );
 }
