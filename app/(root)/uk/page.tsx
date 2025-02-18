11647326860fc5e0c079bce36470eb63c6007d1e
vadimNS <vadik1212a@gmail.com>
2025-02-18 17:39:20 +0200

    commit

--- app/(root)/uk/page.tsx
+++ app/(root)/uk/page.tsx
@@ -9,46 +9,25 @@ interface Props {
 }
 
 const UkTable: React.FC = () => {
-  const [ukData, setUkData] = useState<Props[]>([]);
+  const [UKData, setUKData] = useState<Props[]>([]);
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("/api/uk");
         const data = await res.json();
-        setUkData(data["ukData"] || []);
+        console.log(data);
+        //setUKData(data["hongKongData"]);
+        //setFilteredData(data["hongKongData"]);
       } catch (error) {
         console.log(error);
-        setUkData([]);
       }
       setLoading(false);
     };
 
     fetchData();
   }, []);
-
-  if (loading) {
-    return <div>Завантаження...</div>;
-  }
-
-  return (
-    <div>
-      {ukData.length === 0 ? (
-        <div>Немає даних</div>
-      ) : (
-        ukData.map((item) => (
-          <div key={item.id}>
-            <p>ID: {item.id}</p>
-            <p>Назва ліцензії: {item.licenseName}</p>
-            <p>Адреса: {item.address}</p>
-            <p>Тип адреси: {item.addressType}</p>
-            <hr />
-          </div>
-        ))
-      )}
-    </div>
-  );
+  return <div>Test</div>;
 };
 
 export default UkTable;
--- app/api/uk/route.ts
+++ app/api/uk/route.ts
@@ -2,9 +2,8 @@ import { prisma } from "@/prisma/prisma-client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const hongKongData = await prisma.hongKong.findMany();
+  const UKData = await prisma.hongKong.findMany();
   return NextResponse.json({
-    hongKongData,
+    UKData,
   });
 }
-
